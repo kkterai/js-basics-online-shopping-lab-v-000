d3bccc5cc81a9288a74b9600f4fc8eb1d314b946
Kenlyn Terai <kkterai@gmail.com>
2020-02-11 16:56:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,45 +15,45 @@ function addToCart(item) {
   cart.push(object);
   return `${item} has been added to your cart`
 }
-
-function viewCart() {
-  var arr = [];
-  for( var i = 0; i< cart.length; i++){
-     arr.push(`${cart[i].itemName} at $${cart[i].itemPrice}`);
-
-     if(cart.length === 0){
-       return "Your shopping cart is empty.";
-     } else if(cart.length === 1) {
-       return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
-     } else if(cart.length === 2) {
-       return  `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
-     }else{
-       var last = arr.pop();
-       return `In your cart, you have ${arr.join(', ')}, and ${last}.`;
-     }
-  }
-}
-
-function total() {
-  var ar = [];
-  for(var i = 0; cart.length > i; i++){
-    ar.push(cart[i].itemPrice);
-  }
-  return ar.reduce((acc, val) => acc + val, 0);
-}
-
-
-function removeFromCart(item) {
-  for(var i = 0; cart.length > i; i++) {
-    if(item === cart[i].itemName){
-      cart.splice(i, 1);
-      return cart;
-    } else {
-      return `That item is not in your cart.`;
-    }
-  }
-}
-
-function placeOrder(cardNumber) {
-  // write your code here
-}
+//
+// function viewCart() {
+//   var arr = [];
+//   for( var i = 0; i< cart.length; i++){
+//      arr.push(`${cart[i].itemName} at $${cart[i].itemPrice}`);
+//
+//      if(cart.length === 0){
+//        return "Your shopping cart is empty.";
+//      } else if(cart.length === 1) {
+//        return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
+//      } else if(cart.length === 2) {
+//        return  `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
+//      }else{
+//        var last = arr.pop();
+//        return `In your cart, you have ${arr.join(', ')}, and ${last}.`;
+//      }
+//   }
+// }
+//
+// function total() {
+//   var ar = [];
+//   for(var i = 0; cart.length > i; i++){
+//     ar.push(cart[i].itemPrice);
+//   }
+//   return ar.reduce((acc, val) => acc + val, 0);
+// }
+//
+//
+// function removeFromCart(item) {
+//   for(var i = 0; cart.length > i; i++) {
+//     if(item === cart[i].itemName){
+//       cart.splice(i, 1);
+//       return cart;
+//     } else {
+//       return `That item is not in your cart.`;
+//     }
+//   }
+// }
+//
+// function placeOrder(cardNumber) {
+//   // write your code here
+// }
